838c8d8768476e4a9968f887a5e8e8aba15fb394
PalagiriNagayogeswari <yogi34343@gmail.com>
2026-07-05 12:01:55 +0530

    [Nagayogeswari]:Implemented UC1 of hashtable of implementing hahstable using linkedlist and printed the frequency of each word in a sentence

---
+++ HashTable.java
@@ -0,0 +1,79 @@
+
+//created a class MyMapNode to implement hashtable using linkedlist.
+class MyMapNode {
+
+    String key;
+    int value;
+    MyMapNode next;
+
+    public MyMapNode(String key, int value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+    }
+}
+
+//MyHashTable class to implement hashtable.
+class MyHashTable {
+
+    MyMapNode head;
+
+    public void add(String key) {
+
+        if (head == null) {
+            head = new MyMapNode(key, 1);
+            return;
+        }
+
+        MyMapNode curr = head;
+
+        while (curr != null) {
+
+            if (curr.key.equals(key)) {
+                curr.value++;
+                return;
+            }
+
+            if (curr.next == null)
+                break;
+
+            curr = curr.next;
+        }
+
+        curr.next = new MyMapNode(key, 1);
+    }
+
+    public void print() {
+
+        if (head == null) {
+            System.out.println("Table is Empty");
+            return;
+        }
+
+        MyMapNode curr = head;
+
+        while (curr != null) {
+            System.out.println(curr.key + " -> " + curr.value);
+            curr = curr.next;
+        }
+    }
+}
+
+//Main class implementing the hashtable 
+public class HashTable {
+
+    public static void main(String[] args) {
+
+        String sentence = "To be or not to be";
+
+        String[] words = sentence.split(" ");
+
+        MyHashTable hashTable = new MyHashTable();
+
+        for (String word : words) {
+            hashTable.add(word);
+        }
+
+        hashTable.print();
+    }
+}
